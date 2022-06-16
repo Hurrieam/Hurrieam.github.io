2a189e16d743d3558778920bb84ce3cb680d3097
Hurrieam Hu <1286499252@qq.com>
2022-06-16 09:38:00 +0800

    letter pwd check
    letter pwd check

--- 19th-birthday-letter/scripts/script.js
+++ 19th-birthday-letter/scripts/script.js
@@ -1,5 +1,20 @@
 "use strict";
 $(document).ready(function () {
+    // password checker
+    for (;;) {
+        var password = prompt("此网站小曹已查看哦，请输入小曹专属口令～\n输入“b”返回主页");
+        if ((password === null || password === void 0 ? void 0 : password.toString()) === "我爱小胡") {
+            alert("欢迎你～");
+            break;
+        }
+        else {
+            if ((password === null || password === void 0 ? void 0 : password.toString()) === "b") {
+                window.location.href = "../index.html";
+                break;
+            }
+            alert("口令错误～");
+        }
+    }
     $("#flip").click(function () {
         $("#panel").slideDown("slow");
         $("#flip").fadeOut("slow");
--- 19th-birthday-letter/scripts/script.ts
+++ 19th-birthday-letter/scripts/script.ts
@@ -1,4 +1,21 @@
 $(document).ready(() => {
+  // password checker
+  for (;;) {
+    let password: string | null = prompt(
+      "此网站小曹已查看哦，请输入小曹专属口令～\n输入“b”返回主页"
+    );
+    if (password?.toString() === "我爱小胡") {
+      alert("欢迎你～");
+      break;
+    } else {
+      if (password?.toString() === "b") {
+        window.location.href = "../index.html";
+        break;
+      }
+      alert("口令错误～");
+    }
+  }
+
   $("#flip").click(() => {
     $("#panel").slideDown("slow");
     $("#flip").fadeOut("slow");
